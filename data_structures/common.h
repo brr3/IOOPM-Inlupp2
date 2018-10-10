975f2ebef013eb994b78df28d5e5aa853cb048f9
brr3 <36078418+brr3@users.noreply.github.com>
2018-10-10 11:50:57 +0200

    Create common.h

---
+++ data_structures/common.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stdbool.h>
+#include "dbg.h"
+
+typedef union elem elem_t;
+
+union elem
+{
+  int i;
+  unsigned int u;
+  bool b;
+  float f;
+  void *v;
+  char *s;
+};
+
+typedef int (*ioopm_hash_function)(elem_t t);
+typedef bool (*ioopm_eq_function)(elem_t a, elem_t b);
+typedef bool (*ioopm_predicate)(elem_t key, elem_t value, void *extra);
+/// DODGE: sending in elem, not pointer to it - thus only able to change elem of type char* or void*
+typedef void (*ioopm_apply_function)(elem_t key, elem_t value, void *extra);
