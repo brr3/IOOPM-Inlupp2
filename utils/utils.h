7eec18bfa85451c19b1d62bdebb36ed14c099246
brr3 <36078418+brr3@users.noreply.github.com>
2018-10-10 12:14:26 +0200

    Update utils.h

---
+++ utils/utils.h
@@ -0,0 +1,53 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <math.h>
+
+extern char *strdup(const char *);
+
+
+typedef union { // Not: undvik gärna så här korta namn på variabler! Varför?
+	int   int_value;
+	float float_value;
+	char *string_value;
+	char  char_value;
+} answer_t;
+
+typedef bool(*check_func)(char *);
+
+typedef answer_t(*convert_func)(char *);
+
+struct item
+{
+	char *item_name;
+	char *item_description;
+	int   item_price;
+	char *item_shelf;
+	int   item_amount;
+};
+
+typedef struct item item_t;
+
+
+bool not_empty(char *str);
+bool is_number(char *str);
+bool is_float(char *str);
+bool is_menu_selection(char *str);
+
+int read_string(char *buf, int buf_siz);
+int ask_question_int(char *question);
+int ask_question_amount(char *question);
+char *ask_question_string(char *question);
+char *ask_question_shelf(char *question);
+char ask_question_menu(char *question);
+double ask_question_float(char *question);
+
+answer_t ask_question(char *question, check_func check, convert_func convert);
+answer_t make_float(char *);
+
+#endif
